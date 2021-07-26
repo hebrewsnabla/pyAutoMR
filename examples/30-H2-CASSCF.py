2be3486a244985f2378abba1ae34562cb2e25060
hebrewsnabla <srwang20@fudan.edu.cn>
2021-07-26 14:24:57 +0800

    add examples

---
+++ examples/30-H2-CASSCF.py
@@ -0,0 +1,17 @@
+from pyscf import lib
+#from pyphf import util, guess
+from automr import guess, autocas, cidump
+
+lib.num_threads(4)
+
+xyz = 'H 0.0 0.0 0.0; H  0.0 0.0 1.9' #sys.argv[1]
+#fch = 'n2.fchk' #sys.argv[2]
+bas = 'cc-pvdz'
+
+mf = guess.mix(xyz, bas, conv='tight')
+#mf = guess.from_frag(xyz, bas, [[0],[1]], [0,0], [3,-3], cycle=50)
+#mf = guess.check_stab(mf)
+
+mf2 = autocas.cas(mf)
+#cidump.dump(mf2)
+
